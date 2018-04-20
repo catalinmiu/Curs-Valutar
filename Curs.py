1c4b3da9b4c4930eba7f833c55b9ef359cbedee1
Catalin Miu <catalinmiu_98@yahoo.com>
2018-04-20 10:36:35 +0300

    other little cahnges:)

--- Curs.py
+++ Curs.py
@@ -16,25 +16,27 @@ class Json:
                 a file with this pattern name 'currency[day_of_month].json'.
                 Otherwise, the day will be the current day
         """
-        myfile = 'currency' + str(day_of_month) + '.json'
-        my_file = Path(myfile)
-        if my_file.is_file():
-            return 1
+        file_name = 'currency' + str(day_of_month) + '.json'
+        file_name = Path(file_name)
+        if file_name.is_file():
+            return True
         else:
-            return 0
+            return False
 
     def get_json(self):
-        if self.is_json_locally()==0:
+        if not self.is_json_locally():
             self.json = json.load(urlopen(
                 'https://openexchangerates.org/api/latest.json?app_id=eb234aa07f334a5aa05955f7a35f45d0'))
-            print("nu este")
-        else:
-            print("ie")
 
     def copy_json_locally(self):
         file_name = 'currency' + str(datetime.datetime.today().day) + '.json'
         with open(file_name, 'w') as file:
             json.dump(self.json, file, ensure_ascii=False)
+
+
+class CurrencyPrompt:
+    def __init__(self):
+        pass
 
 
 json1 = Json()
